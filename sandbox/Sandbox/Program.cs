8147cd46ef11a83dd625b7f4b3d6e7f8e561ec26
moorev1 <vincentvangeemoore@yahoo.com>
2023-09-16 23:40:10 +0000

    add message

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Console.WriteLine("Hello World by me!");
+        Console.WriteLine("Enter Your First Name");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Enter Your Last Name");
+        string lastName = Console.ReadLine();
+        string fullName = $"{lastName}, {firstName}, {lastName}";
+        Console.WriteLine($"Your name is {fullName}");
     }
 }
